777bc02123290d6005b86eb99ffe7da97ca185c6
Hauke Bartsch <HaukeBartsch@gmail.com>
2022-05-31 17:22:31 +0200

    add the usual meta tags

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/style.css"></link>
-
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Visualizing DICOM structures">
+    <meta name="keywords" content="D3, DICOM">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"></link>
+</head>
 <body>
 	<script src="js/es5-shim.js"></script>
 	<script src="js/es6-shim.js"></script>
