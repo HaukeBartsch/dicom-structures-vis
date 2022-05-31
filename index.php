2f487d7edb602b7fe8f7eae92f0c04e9bdbd639c
Hauke Bartsch <HaukeBartsch@gmail.com>
2022-05-31 17:17:35 +0200

    make remote scripts local

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
-<link rel="stylesheet" href="css/colorbrewer2.css"></link>
 <link rel="stylesheet" href="css/style.css"></link>
 
 <body>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.0/es6-shim.js"></script>
+	<script src="js/es5-shim.js"></script>
+	<script src="js/es6-shim.js"></script>
 	<script src="js/d3.v3.min.js"></script>
 	<script src="js/narrative.js"></script>
 	<script src="js/charts.js"></script>
